345340d319a38c6840349803e6de67a9ace0041d
feliperromao <feliperomaocad@gmail.com>
2025-01-27 18:34:23 -0300

    fix: improve security rules

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+const API_URL = `${process.env.REACT_APP_BACKEND_GRAPH_API}/auth/profile`
 
 interface ProtectedRouteProps {
   element: React.ReactElement;
@@ -8,15 +10,29 @@ interface ProtectedRouteProps {
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
   const location = useLocation();
 
-  //TODO: check if exists user data and token in localstorage
   const user = localStorage.getItem('user');
   const token = localStorage.getItem('auth-token');
 
-  //TODO: make reqiest to validate token
-
-  if ((!user || !token )) {
+  if ((!user || !token)) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
+
+  //TODO: make reqiest to validate token
+  axios.get(API_URL, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Accept': 'application/json'
+    }
+  }).then(({data}) => {
+    console.log("🚀 ~ data:", data)
+    if (data.type != "PERSONAL") {
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+  }).catch(() => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('auth-token');
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  })
 
   return element;
 };
