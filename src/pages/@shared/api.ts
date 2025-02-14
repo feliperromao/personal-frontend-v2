58513d1538b9404fb2ad8c30cb6117d2db8adc3d
feliperromao <feliperomaocad@gmail.com>
2025-02-14 11:56:28 -0300

    refactor: remove comments

--- src/pages/@shared/api.ts
+++ src/pages/@shared/api.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_API, // Define a URL base automaticamente
+  baseURL: process.env.REACT_APP_BACKEND_API,
 });
 
-// Interceptor para adicionar o token automaticamente
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("auth-token");
